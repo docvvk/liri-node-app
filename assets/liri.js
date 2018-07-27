11e342b9a19716771d25981bd709840db7ae6355
Vivek Malhan <vivek.malhan@yahoo.com>
2018-07-27 14:38:24 -0400

    final commit

--- assets/liri.js
+++ assets/liri.js
@@ -3,46 +3,17 @@ require("dotenv").config();
 const chalk = require('chalk');
 const chalkAnimation = require('chalk-animation');
  
-chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
-const miles = 18;
-const calculateFeet = miles => miles * 5280;
-
-console.log(chalk`
-  There are {bold 5280 feet} in a mile.
-  In {bold ${miles} miles}, there are {green.bold ${calculateFeet(miles)} feet}.
-`);
-
-console.log(chalk.keyword('skyblue').bgRed.bold('Hello World!'));
-
-
 var keys = require('./config');
 var fs = require('fs');
-
-
 var request = require('request');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-var userINPUT = process.argv[3];
- 
-
-// console.log(client);
-// console.log(spotify);
 
 var getTweets = () => {
     var client = new Twitter(keys.twitterKeys);
-    // var client = new Twitter({
-    //     consumer_key: 'CCt3y4nxeyQJpMrQvWgA76VoS',
-    //     consumer_secret: '7Lg0asU9U23U8pbUTDhxpAz4NGMFkP61wKOjiefGGsRTzJJMYY',
-    //     access_token_key: '112109869-wrcqXO6EDllx6feG7F5pH9KJIq7MbPdbCgatFQ4x',
-    //     access_token_secret: 'eq7knAONc7hHi5SzFKanSLVQOA4ONrvJtG6ZEfzQObEaE'
-    // });
-    var twitterUsername = userINPUT;
     
-    var params = {screen_name: twitterUsername, count: 1};
-    if (!twitterUsername) {
-        twitterUsername = 'shaurya';
-    }
-
+    var params = {screen_name: 'shaurya', count: 1};
+  
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (error) {
             console.log(error);
@@ -58,7 +29,7 @@ var getTweets = () => {
 
             console.log(chalk.bgYellow.blue(JSON.stringify(data, null, 2)));
             // console.log(response);
-            // dataToLog(data);
+            dataToLog(data);
             console.log(chalk.bold.red("------------------------------------------------------------------------------------"));
             var rainbow = chalkAnimation.rainbow('log.txt updated !');
             setTimeout(()=> {
@@ -76,8 +47,7 @@ var getSong = (variable) => {
     if (!variable) {
         variable = "The+Sign+Ace+of+Base"
     };
-    console.log(variable);
-
+    // console.log(variable);
 
     var spotifyParams = {type: 'track', query: variable}
     spotify.search(spotifyParams, (err,data) => {
@@ -94,16 +64,15 @@ var getSong = (variable) => {
                 album: data.tracks.items[i].album.name
             });
             console.log(chalk.yellow(JSON.stringify(dataArray, null, 2)));
-            // dataToLog(dataArray);
+            dataToLog(dataArray);
             console.log(chalk.red("--------------------"));
-            var rainbow = chalkAnimation.rainbow("log.txt was updated");
+            var rainbow = chalkAnimation.rainbow("log.txt updated");
             setTimeout(() => {
                 rainbow.stop(); // Animation stops
             }, 3000);
         }
     });
 }
-// getSong();
 
 var movieInfo = (variable) => {
     if (!variable) {
@@ -126,7 +95,7 @@ var movieInfo = (variable) => {
                 actors: "Actors: " + info.Actors
             });
             console.log(chalk.keyword('pink')(JSON.stringify(dataArray, null, 2)));
-            // dataToLog(dataArray);
+            dataToLog(dataArray);
             console.log(chalk.red("--------------------------------"));
             var rainbow = chalkAnimation.rainbow("log.txt updated");
             setTimeout(() => {
@@ -135,44 +104,43 @@ var movieInfo = (variable) => {
         }
     })
 }
-// movieInfo();
- var doRandom = () => {
-     fs.readFile("random.txt", "utf8", (err,data) => {
-         if (err) {
-             return console.log(err)
-         };
 
-         var dataArray = data.split(",");
-         console.log(dataArray);
-
-         switch (dataArray[0]) {
-             case "my-tweets":
-             getTweets();
-             break;
-             case "spotify-this-song":
-             getSong(dataArray[1]);
-             break;
-             case "movie-this":
-             movieInfo(dataArray[1]);
-             break;
-             default:
-             var glitch = chalkAnimation.glitch("The text in random.txt is indecipherable !");
-             setTimeout(() => {
-                 glitch.stop();
-             }, 3000);
-         }
-     })
+var doRandom = () => {
+    fs.readFile("random.txt", "utf8", (err,data) => {
+        if (err) {
+            return console.log(err)
+        };
+        var dataArray = data.split(",");
+        console.log(dataArray);
+        switch (dataArray[0]) {
+            case "my-tweets":
+            getTweets();
+            break;
+            case "spotify-this-song":
+            getSong(dataArray[1]);
+            break;
+            case "movie-this":
+            movieInfo(dataArray[1]);
+            break;
+            default:
+            var glitch = chalkAnimation.glitch("The text in random.txt is indecipherable !");
+            setTimeout(() => {
+                glitch.stop();
+            }, 3000);
+        }
+    })
  }
- var dataToLog = () => {
-     // Appends entered data to a new file named log
-     for (var j = 0; j < data.length; j++) {
-         fs.appendFile("log.txt", JSON.strinf=gify(data[j], null, 2) + "\r\n", (err) => {
-             if (err) {
-                 return console.log(err);
-             }
-         });
-     }
- }
+ 
+var dataToLog = (data) => {
+    // Appends entered data to a new file named log
+    for (var j = 0; j < data.length; j++) {
+        fs.appendFile("log.txt", JSON.stringify(data[j], null, 2) + "\r\n", (err) => {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    }
+}
  // Allows for multiple words to be in song/movie title
 var userInput = process.argv.slice(3).join("+");
 console.log(userInput)
@@ -181,10 +149,10 @@ switch (process.argv[2]) {
     getTweets();
     break;
     case "spotify-this-song":
-    getSpotify(userInput);
+    getSong(userInput);
     break;
     case "movie-this":
-    getMovie(userInput);
+    movieInfo(userInput);
     break;
     case "do-what-it-says":
     doRandom();
