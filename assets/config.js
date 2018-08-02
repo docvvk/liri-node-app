4ad427d84f9a83c615e5575cc30316b3ce09b287
Vivek <36576957+docvvk@users.noreply.github.com>
2018-08-02 11:15:11 -0400

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,17 +1,5 @@
 console.log('this is loaded');
 
-// exports.twitterKeys = {
-//   consumer_key: 'CCt3y4nxeyQJpMrQvWgA76VoS',
-//   consumer_secret: '7Lg0asU9U23U8pbUTDhxpAz4NGMFkP61wKOjiefGGsRTzJJMYY',
-//   access_token_key: '112109869-wrcqXO6EDllx6feG7F5pH9KJIq7MbPdbCgatFQ4x',
-//   access_token_secret: 'eq7knAONc7hHi5SzFKanSLVQOA4ONrvJtG6ZEfzQObEaE'
-// };
-
-// exports.spotifyKeys = {
-//   id: 'c3dde4750aab43a09095da2bf52641b5',
-//   secret: '6cea179d3ab046068984475d63a0e511'
-// };
-
 exports.twitterKeys = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
